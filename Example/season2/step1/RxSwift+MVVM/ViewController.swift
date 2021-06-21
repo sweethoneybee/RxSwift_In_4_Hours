7b8e5d920cdb4574ff0b88bbafe0d13b498c6c27
sweethoneybee <jsjphone8@gmail.com>
2021-06-21 21:56:58 +0900

    영상따라가며 빈 곳 RxSwift + MVVM 으로 채우기

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -15,14 +15,14 @@ let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
     }
-
+    
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
         guard let v = v else { return }
         UIView.animate(withDuration: 0.3, animations: { [weak v] in
@@ -31,20 +31,37 @@ class ViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
-
+    
+    func downloadJson(_ url: String, _ complete: @escaping ((String?) -> Void)) {
+        
+        let url = URL(string: url)!
+        let task = URLSession.shared.dataTask(with: url) { data, _, err in
+            guard err == nil else {
+                return
+            }
+            
+            if let dat = data, let json = String(data: dat, encoding: .utf8) {
+                complete(json)
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
     // MARK: SYNC
-
+    
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
-
+    
     @IBAction func onLoad() {
         editView.text = ""
         setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
         
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        downloadJson(MEMBER_LIST_URL) { json in
+            DispatchQueue.main.async {
+                self.editView.text = json
+                self.setVisibleWithAnimation(self.activityIndicator, false)
+            }}
+        
     }
 }
---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by 정성훈 on 2021/06/21.
+//  Copyright © 2021 iamchiwon. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+class MenuListViewModel {
+    var menuObservable = BehaviorRelay<[Menu]>(value: [])
+    
+    lazy var itemsCount = menuObservable.map {
+        $0.map { $0.count }.reduce(0, +)
+    }
+    
+    lazy var totalPrice = menuObservable.map {
+        $0.map { $0.price * $0.count }.reduce(0, +)
+    }
+    
+    init() {
+        _ = APIService.fetchAllMenusRx()
+            .map { data -> [MenuItem] in
+                struct Response: Decodable {
+                    let menus: [MenuItem]
+                }
+                let response = try! JSONDecoder().decode(Response.self, from: data)
+                
+                return response.menus
+            }
+            .map { menuItems in
+                var menus: [Menu] = []
+                menuItems.enumerated().forEach { (index, item) in
+                    let menu = Menu.fromMenuItems(id: index, item: item)
+                    menus.append(menu)
+                }
+                return menus
+            }
+            .take(1)
+            .bind(to: menuObservable)
+    }
+    
+    func onOrder() {
+        
+    }
+    
+    func clearAllItemSelections() {
+        _ = menuObservable
+            .map { menus in
+                menus.map { m in
+                    Menu(id: m.id, name: m.name, price: m.price, count: 0)
+                }
+            }
+            .take(1)
+            .subscribe(onNext: {
+                self.menuObservable.accept($0)
+            })
+    }
+    
+    func changeCount(item: Menu, increase: Int) {
+        _ = menuObservable
+            .map { menus in
+                menus.map { m in
+                    if m.id == item.id {
+                        return Menu(id: m.id,
+                                    name: m.name,
+                                    price: m.price,
+                                    count: max(m.count + increase, 0))
+                    } else {
+                        return Menu(id: m.id,
+                                    name: m.name,
+                                    price: m.price,
+                                    count: m.count)
+                    }
+                }
+            }
+            .take(1)
+            .subscribe(onNext: {
+                self.menuObservable.accept($0)
+            })
+    }
+}
